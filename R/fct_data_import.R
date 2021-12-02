45ad5c37c9be19c654ca18bc7b730d11152cfad2
Sterre Siekman <s.l.siekman@lumc.nl>
2021-12-02 19:10:56 +0100

    added documentation for some functions

--- R/fct_data_import.R
+++ R/fct_data_import.R
@@ -19,7 +19,51 @@ detect_plate_and_well <- function(data) {
     relocate(plate_well, .after = sample_name)
 }
 
-read_non_rectangular <- function(path, delim) {
+#' Get block
+#'
+#' @param data 
+#' @param variable 
+#' @param name_specific 
+#' @param name_total 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' get_block(LacyTools_summary, variable = "Absolute Intensity (Background Subtracted, 2+)")
+#' 
+get_block <- function(data, variable, name_specific = "Spike", name_total = "Total") {
+  rows <- find_block(data, variable)
+  block <- data[rows, ]
+  colnames(block) <- unlist(block[1, ])
+  colnames(block)[1] <- "sample_name"
+  better_name_output <- str_remove_all(str_replace_all(tolower(variable), " ", "_"),
+                                       "[\\(\\)\\,\\/\\[\\]]")
+  block <- block[-c(1, 2, 3), ] %>% 
+    mutate(lacytools_output = better_name_output) %>% 
+    mutate(across(-c(sample_name, lacytools_output), as.numeric)) %>% 
+    select(-where(function(x) all(is.na(x))))
+  block <- detect_group(block, name_specific, name_total)
+  return(block)
+}
+
+#' Read in non-rectangular delimited files
+#' 
+#' \code{read_non_rectangular()} can read flat files where the number of fields per line is 
+#' not constant (non-rectangular data). Blank lines in the field are not skipped 
+#' and empty fields "" are interpreted as \code{NA}. This function was created to read
+#' LacyTools summary files (tab-delimited .txt file).
+#'
+#' @param path A path to a file with non-rectangular data.
+#' @param delim The field separator used in the file. 
+#'              For example, use "\t" for files with tab-separated values 
+#'              and "," or ";" for files with comma-separated values (.csv).
+#'
+#' @return A data frame (\code{\link{[base]data.frame}}) containing the data in the flat file.
+#' @export
+#'
+#' @examples
+read_non_rectangular <- function(path, delim = "\t") {
   lines <- readLines(path, n = -1, ok = TRUE)
   columns <- stringr::str_split(lines, pattern = delim)
   n_columns <- max(purrr::map_int(columns, ~ length(.x)))
