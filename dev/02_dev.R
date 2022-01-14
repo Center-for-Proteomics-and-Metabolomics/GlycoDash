04e35c171c0ce09dcab27008da3cdd544f651ca8
Sterre Siekman <s.l.siekman@lumc.nl>
2022-01-14 12:32:01 +0100

    Added a cleaned up version of the metadata where sample ID's match those in platedesign

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -58,6 +58,7 @@ golem::add_css_file( "custom" )
 ## Add internal datasets ----
 ## If you have data in your package
 usethis::use_data_raw( name = "example_LacyTools_summary", open = TRUE )
+usethis::use_data_raw(name = "metadata_cleaned", open = TRUE)
 
 ## Tests ----
 ## Add one line by test you want to create
