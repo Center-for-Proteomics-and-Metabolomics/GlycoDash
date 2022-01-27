e588ac50f3fb2903c341b42ebc6c8757666ed593
Sterre Siekman <s.l.siekman@lumc.nl>
2022-01-27 22:25:18 +0100

    Created documentation/tests for some functions in fct_spectra_curation.R

--- R/fct_spectra_curation.R
+++ R/fct_spectra_curation.R
@@ -2,7 +2,7 @@
 #'
 #' \code{do_criteria_check()} performs an analyte quality criteria check for
 #' every spectrum in the data. This function is used within the function
-#' \code{create_spectra_check_data_frame()}.
+#' \code{\link{summarize_spectra_checks()}}.
 #'
 #' @param data A dataframe in long format (one row for each analyte + sample
 #'   combination).
@@ -52,8 +52,78 @@ do_criteria_check <- function(data,
                                    max_ppm_deviation)
   IPQ_check <- data$isotopic_pattern_quality <= max_ipq
   sn_check <- data$sn >= min_sn
-  all_checks <- mass_acc_check & IPQ_check & sn_check %>% 
-    tidyr::replace_na(FALSE)
+  all_checks <- (mass_acc_check & IPQ_check & sn_check) %>% 
+    tidyr::replace_na(., FALSE)
   return(all_checks)
 }
 
+#' Summarize analyte quality criteria checks
+#'
+#' \code{summarize_spectra_checks()} first performs analyte quality criteria checks
+#' for every spectrum in the data using \code{\link{do_criteria_check()}}. Then it
+#' calculates the proportion of passing analytes per spectrum and the sum
+#' intensity of passing analytes per spectrum.
+#'
+#' @inheritParams do_criteria_check
+#'
+#' @return A dataframe in which each row represents one spectrum. The dataframe
+#' contains five columns: 
+#' \describe{
+#'     \item{sample_name}{The name of the sample for which this spectrum was recorded.}
+#'     \item{sample_type}{The type of sample (e.g. negative control, blank etc.).}
+#'     \item{group}{The group (Total or Specific) that this sample belongs to.}
+#'     \item{passing_proportion}{The proportion of analytes that 
+#'     passed the criteria checks in this spectrum.}
+#'     \item{sum_intensity}{The sum intensity of all passing analytes in this spectrum}
+#'     }
+#' @export
+#'
+#' @examples
+#' data("long_data")
+#' summarize_spectra_checks(data = long_data,
+#'                                min_ppm_deviation = -20,
+#'                                max_ppm_deviation = 20,
+#'                                max_ipq = 0.2,
+#'                                min_sn = 9)
+summarize_spectra_checks <- function(data, min_ppm_deviation, max_ppm_deviation, max_ipq, min_sn) {
+  required_columns <- list("group",
+                           "sample_type",
+                           "sample_name")
+  
+  if(any(!(required_columns %in% colnames(data)))) {
+    missing_columns <- required_columns[!(required_columns %in% colnames(data))]
+    stop(paste0("The data doesn't contain the required column(s) ",
+                paste0(missing_columns,
+                       collapse = " and "),
+                "."))
+  }
+  
+  spectra_check <- data %>% 
+    dplyr::mutate(criteria_check = do_criteria_check(data,
+                                                     min_ppm_deviation = min_ppm_deviation,
+                                                     max_ppm_deviation = max_ppm_deviation,
+                                                     max_ipq = max_ipq,
+                                                     min_sn = min_sn)) %>% 
+    dplyr::group_by(group, sample_type, sample_name) %>% 
+    dplyr::summarise(passing_proportion = sum(criteria_check)/dplyr::n(), 
+                     sum_intensity = sum(absolute_intensity_background_subtracted[criteria_check == TRUE]))
+  return(spectra_check)
+}
+
+#' Calculate the population standard deviation.
+#'
+#' @param x A numeric vector or an R object but not a factor coercible to numeric by as.double(x).
+#' @param na.rm Logical that indicates whether missing values should be removed.
+#'
+#' @return The population standard deviation (numeric).
+#' @export
+#'
+#' @examples
+#' numbers <- c(2, 5, 3, 9, 6)
+#' sd_p(numbers)
+#' 
+#' numbers_na <- c(2, 5, 3, 9, 6, NA)
+#' sd_p(numbers, na.rm = TRUE)
+sd_p <- function(x, na.rm = FALSE) {
+  sd(x, na.rm = na.rm) * sqrt((length(x)-1)/length(x))
+}
--- tests/testthat/test-spectra_curation.R
+++ tests/testthat/test-spectra_curation.R
@@ -1,7 +1,8 @@
 library(glycodash)
 
-test_that("do_criteria_check function arguments fulfill the requirements.", {
-  data("long_data")
+data("long_data")
+
+test_that("do_criteria_check() arguments fulfill the requirements.", {
   expect_error(do_criteria_check(data = long_data, 
                                  min_ppm_deviation = "oops",
                                  max_ppm_deviation = "this is not numeric",
@@ -9,14 +10,38 @@ test_that("do_criteria_check function arguments fulfill the requirements.", {
                                  min_sn = as.Date.character("02-01-1997",
                                                             format = "%d-%m-%Y")), 
                "One or more quality criteria arguments are non-numeric.")
-  
   wrong_data <- long_data %>% 
     dplyr::select(-sn)
-  
   expect_error(do_criteria_check(data = wrong_data, 
                                  min_ppm_deviation = -20,
                                  max_ppm_deviation = 20,
                                  max_ipq = 0.2,
                                  min_sn = 9), 
                "The data doesn't contain the required columns with the quality criteria.")
+})
+
+test_that("do_criteria_check() returns a logical vector without NA's.", {
+  data("long_data")
+  criteria_check <- do_criteria_check(data = long_data, 
+                                      min_ppm_deviation = -20,
+                                      max_ppm_deviation = 20,
+                                      max_ipq = 0.2,
+                                      min_sn = 9)
+  expect_type(criteria_check,
+              "logical")
+  expect_false(anyNA(criteria_check))
+})
+
+test_that("summarize_spectra_checks() verifies the existence of required columns", {
+  data("long_data")
+  wrong_data <- long_data %>% 
+    dplyr::select(-c(sample_name, group))
+  expect_error(summarize_spectra_checks(data = wrong_data,
+                                        min_ppm_deviation = -20,
+                                        max_ppm_deviation = 20,
+                                        max_ipq = 0.2,
+                                        min_sn = 9),
+               regexp = "The data doesn't contain the required column(s) group and sample_name.", 
+               fixed = TRUE)
+  
 })
