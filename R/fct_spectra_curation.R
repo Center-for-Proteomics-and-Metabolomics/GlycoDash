f00421e82b7f09c1285afca07406fa468964b1ae
Sterre Siekman <s.l.siekman@lumc.nl>
2022-01-28 17:24:52 +0100

    Added functions and documentation to fct_spectra_curation.R and wrote tests for these functions.

--- R/fct_spectra_curation.R
+++ R/fct_spectra_curation.R
@@ -1,8 +1,60 @@
+#'Define clusters based on analyte names
+#'
+#'This function determines which cluster an analyte belongs to based on the
+#'analyte name.
+#'
+#'@inheritParams do_criteria_check
+#'@param clusters_regex A vector containing character strings. These strings are
+#'  used as regular expressions to classify analytes into clusters.
+#'  \code{clusters_regex} should contain one character string per cluster in the
+#'  data.
+#'
+#'@return The function returns the original dataframe given as the data
+#'  argument, but with an additional column named "cluster". This character
+#'  column indicates to what cluster the analyte belongs.
+#'@export
+#'
+#' @examples
+#' data("long_data")
+#' define_clusters(data = long_data,
+#'                 clusters_regex = "IgGI1")
+define_clusters <- function(data, clusters_regex) {
+  
+  regex_found <- purrr::map_lgl(
+    clusters_regex,
+    function(regex) {
+      any(stringr::str_detect(string = data$analyte,
+                              pattern = regex))
+                 })
+  
+  if (any(regex_found == FALSE)) {
+    stop(paste("The regular expression(s)",
+               paste0(clusters_regex[!regex_found],
+                      collapse = " and "),
+               "matched no analytes in the \"analyte\" column of the data."))
+  }
+  
+  clusters <- data %>% 
+    tidyr::extract(col = analyte,
+                   into = "cluster",
+                   regex = paste0("(",
+                                  paste0(clusters_regex, 
+                                         collapse = "|"),
+                                  ")"),
+                   remove = FALSE)
+  
+  if (anyNA(clusters$cluster)) {
+    stop(paste("Some analytes could not be assigned into a cluster.",
+               "Please reconsider the regular expressions you gave as clusters_regex."))
+  }
+  return(clusters)
+}
+
 #' Perform an analyte quality criteria check for every spectrum in the data.
 #'
 #' \code{do_criteria_check()} performs an analyte quality criteria check for
 #' every spectrum in the data. This function is used within the function
-#' \code{\link{summarize_spectra_checks()}}.
+#' \code{\link{summarize_spectra_checks}}.
 #'
 #' @param data A dataframe in long format (one row for each analyte + sample
 #'   combination).
@@ -59,23 +111,23 @@ do_criteria_check <- function(data,
 
 #' Summarize analyte quality criteria checks
 #'
-#' \code{summarize_spectra_checks()} first performs analyte quality criteria checks
-#' for every spectrum in the data using \code{\link{do_criteria_check()}}. Then it
-#' calculates the proportion of passing analytes per spectrum and the sum
-#' intensity of passing analytes per spectrum.
+#' \code{summarize_spectra_checks()} first performs analyte quality criteria
+#' checks for every spectrum in the data using \code{\link{do_criteria_check}}.
+#' Then it calculates the proportion of passing analytes per spectrum and the
+#' sum intensity of passing analytes per spectrum.
 #'
 #' @inheritParams do_criteria_check
 #'
-#' @return A dataframe in which each row represents one spectrum. The dataframe
-#' contains five columns: 
-#' \describe{
-#'     \item{sample_name}{The name of the sample for which this spectrum was recorded.}
-#'     \item{sample_type}{The type of sample (e.g. negative control, blank etc.).}
-#'     \item{group}{The group (Total or Specific) that this sample belongs to.}
-#'     \item{passing_proportion}{The proportion of analytes that 
-#'     passed the criteria checks in this spectrum.}
-#'     \item{sum_intensity}{The sum intensity of all passing analytes in this spectrum}
-#'     }
+#' @return A dataframe that contains one row per spectrum for each cluster (
+#'   Thus the number of rows is equal to the number of spectra times the number
+#'   of clusters). The dataframe contains five columns: \describe{
+#'   \item{sample_name}{The name of the sample for which this spectrum was
+#'   recorded.} \item{sample_type}{The type of sample (e.g. negative control,
+#'   blank etc.).} \item{group}{The group (Total or Specific) that this sample
+#'   belongs to.} \item{cluster}{The cluster that the analyte belongs to.}
+#'   \item{passing_proportion}{The proportion of analytes that passed the
+#'   criteria checks in this spectrum.} \item{sum_intensity}{The sum intensity
+#'   of all passing analytes in this spectrum} }
 #' @export
 #'
 #' @examples
@@ -88,7 +140,8 @@ do_criteria_check <- function(data,
 summarize_spectra_checks <- function(data, min_ppm_deviation, max_ppm_deviation, max_ipq, min_sn) {
   required_columns <- list("group",
                            "sample_type",
-                           "sample_name")
+                           "sample_name",
+                           "cluster")
   
   if(any(!(required_columns %in% colnames(data)))) {
     missing_columns <- required_columns[!(required_columns %in% colnames(data))]
@@ -104,7 +157,7 @@ summarize_spectra_checks <- function(data, min_ppm_deviation, max_ppm_deviation,
                                                      max_ppm_deviation = max_ppm_deviation,
                                                      max_ipq = max_ipq,
                                                      min_sn = min_sn)) %>% 
-    dplyr::group_by(group, sample_type, sample_name) %>% 
+    dplyr::group_by(group, sample_type, cluster, sample_name) %>% 
     dplyr::summarise(passing_proportion = sum(criteria_check)/dplyr::n(), 
                      sum_intensity = sum(absolute_intensity_background_subtracted[criteria_check == TRUE]))
   return(spectra_check)
@@ -127,3 +180,115 @@ summarize_spectra_checks <- function(data, min_ppm_deviation, max_ppm_deviation,
 sd_p <- function(x, na.rm = FALSE) {
   sd(x, na.rm = na.rm) * sqrt((length(x)-1)/length(x))
 }
+
+#' Calculate cut-off values for spectra curation
+#'
+#' Calculate the cutoffs for the proportion of passing analytes per spectrum and
+#' for the sum intensity of passing analytes per spectrum. This function is used
+#' within the function \code{\link{curate_spectra}}.
+#'
+#' @param spectra_check The dataframe returned by
+#'   \code{\link{summarize_spectra_checks()}}.
+#' @param group_to_filter The group (Specific or Total) to base spectra curation
+#'   cut-offs on.
+#' @param sample_type_to_filter The sample type to base spectra curation
+#'   cut-offs on.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calculate_cut_offs <- function(spectra_check, group_to_filter, sample_type_to_filter) {
+  if (any(!(group_to_filter %in% spectra_check$group), 
+          !(sample_type_to_filter %in% spectra_check$sample_type))) {
+    stop("The group_to_filter and/or sample_type_to_filter are not present in spectra_check.")
+  } 
+  
+  cut_offs <- spectra_check %>% 
+    # This filter should depend on the choice of the user
+    # Implement check for non-existing sample_types/groups
+    dplyr::filter(sample_type == sample_type_to_filter & group == group_to_filter) %>% 
+    dplyr::summarise(av_prop = mean(passing_proportion, na.rm = FALSE),
+                     sd_prop = sd_p(passing_proportion, na.rm = FALSE),
+                     cut_off_prop = av_prop + (3 * sd_prop),
+                     av_sum_int = mean(sum_intensity, na.rm = FALSE),
+                     sd_sum_int = sd_p(sum_intensity, na.rm = FALSE),
+                     cut_off_sum_int = av_sum_int + (3 * sd_sum_int))
+  return(cut_offs)
+}
+
+#' Perform spectra curation
+#'
+#' \code{curate_spectra()} performs spectra curation on mass spectrometry data.
+#' For each spectrum, analytes are curated based on the quality criteria (mass
+#' accuracy, isotopic pattern quality (IPQ) and signal to noise ratio (S/N)).
+#' Then the proportion of passing analytes and the sum intensity of passing
+#' analytes is calculated for each spectrum (using the function
+#' \code{\link{summarize_spectra_checks}}). Based on the average proportion and
+#' sum intensity in a chosen group of samples that should not pass curation
+#' (e.g. Specific Ig negative control samples), cut-off values for spectra
+#' curation are defined (using the function \code{\link{calculate_cut_offs}}).
+#' All spectra with values above those cut-off values pass the spectra curation.
+#'
+#' @inheritParams do_criteria_check
+#' @inheritParams calculate_cut_offs
+#' @inheritParams define_clusters
+#'
+#' @return The function returns the original dataframe given as the data
+#'   argument, but with an additional column named "passed_curation". This
+#'   logical column is \code{TRUE} for spectra that have passed curation and
+#'   \code{FALSE} for spectra that did not pass curation.
+#' @export
+#'
+#' @examples
+#' data("long_data")
+#' curate_spectra(data = long_data,
+#'                min_ppm_deviation = -20,
+#'                max_ppm_deviation = 20,
+#'                max_ipq = 0.2,
+#'                min_sn = 9,
+#'                group_to_filter = "Spike",
+#'                sample_type_to_filter = "CN")
+curate_spectra <- function(data, clusters_regex, min_ppm_deviation, max_ppm_deviation, 
+                           max_ipq, min_sn, group_to_filter, sample_type_to_filter) {
+  data <- define_clusters(data = data,
+                          clusters_regex = clusters_regex)
+  spectra_check <- summarize_spectra_checks(data = data, 
+                                            min_ppm_deviation = min_ppm_deviation,
+                                            max_ppm_deviation = max_ppm_deviation,
+                                            max_ipq = max_ipq,
+                                            min_sn = min_sn)
+  cut_offs <- calculate_cut_offs(spectra_check = spectra_check,
+                                 group_to_filter = group_to_filter,
+                                 sample_type_to_filter = sample_type_to_filter) %>% 
+    dplyr::ungroup(.) %>% 
+    dplyr::select(-c(sample_type, group))
+  
+  spectra_check <- dplyr::left_join(spectra_check, cut_offs, by = "cluster") %>% 
+    dplyr::ungroup(.)
+  
+  passing_spectra <- spectra_check %>% 
+    dplyr::filter((passing_proportion > cut_off_prop) & (sum_intensity > cut_off_sum_int))
+  
+  data <- data %>% 
+    dplyr::mutate(passed_curation = dplyr::case_when(
+      sample_name %in% passing_spectra$sample_name ~ TRUE,
+      TRUE ~ FALSE))
+  
+  no_NAs <- data %>% 
+    dplyr::filter(dplyr::if_all(.cols = c(mass_accuracy_ppm,
+                                          isotopic_pattern_quality,
+                                          sn),
+                                .fns = ~ !is.na(.x)))
+  
+  if (all(no_NAs$passed_curation == FALSE)) {
+    warning("None of the spectra passed curation.")
+  } else {
+    if (all(no_NAs$passed_curation == TRUE)) {
+      warning("All spectra passed curation.")
+    }
+  }
+  
+  return(data)
+}
+
--- tests/testthat/test-spectra_curation.R
+++ tests/testthat/test-spectra_curation.R
@@ -2,6 +2,21 @@ library(glycodash)
 
 data("long_data")
 
+test_that("define_clusters() throws error when any element of clusters_regex results in zero matches", {
+  expect_error(define_clusters(data = long_data,
+                               clusters_regex = c("nonsense",
+                                                  "non-existent")),
+               "The regular expression\\(s\\) nonsense and non-existent matched no analytes in the \"analyte\" column of the data\\.")
+})
+
+test_that("define_clusters() throws an error when any analytes in the data don't match with any of the regular expressions", {
+  df <- data.frame(analyte = c("IgGI1H5N3", "IgGII1H5N3", "unmatched"),
+                   number = c(1:3))
+  expect_error(define_clusters(data = df,
+                               clusters_regex = c("IgGI1", "IgGII1")),
+               "Some analytes could not be assigned into a cluster\\. Please reconsider the regular expressions you gave as clusters_regex\\.")
+})
+
 test_that("do_criteria_check() arguments fulfill the requirements.", {
   expect_error(do_criteria_check(data = long_data, 
                                  min_ppm_deviation = "oops",
@@ -21,7 +36,6 @@ test_that("do_criteria_check() arguments fulfill the requirements.", {
 })
 
 test_that("do_criteria_check() returns a logical vector without NA's.", {
-  data("long_data")
   criteria_check <- do_criteria_check(data = long_data, 
                                       min_ppm_deviation = -20,
                                       max_ppm_deviation = 20,
@@ -41,7 +55,47 @@ test_that("summarize_spectra_checks() verifies the existence of required columns
                                         max_ppm_deviation = 20,
                                         max_ipq = 0.2,
                                         min_sn = 9),
-               regexp = "The data doesn't contain the required column(s) group and sample_name.", 
-               fixed = TRUE)
+               regexp = "The data doesn't contain the required column\\(s\\) group and sample_name and cluster\\.")
   
 })
+
+test_that("summarize_spectra_checks() returns one row per cluster per spectrum", {
+  data("long_data")
+  long_data <- define_clusters(data = long_data,
+                               clusters_regex = "IgGI1")
+  to_replace <- sample(1:69888, 69888/2)
+  long_data$cluster[to_replace] <- "IgGII1"
+  expect_equal(nrow(summarize_spectra_checks(data = long_data,
+                                        min_ppm_deviation = -20,
+                                        max_ppm_deviation = 20,
+                                        max_ipq = 0.2,
+                                        min_sn = 9)),
+               length(unique(long_data$cluster)) * length(unique(long_data$sample_name)))
+})
+
+test_that("curate_spectra() issues warnings if all/no spectra pass curation.", {
+  # If quality criteria are very lenient, even the spectra that cut-offs are
+  # based on (spectra that should not pass) pass the quality criteria checks.
+  # This leads to very high cut-offs, which causes all spectra to fail curation:
+  expect_warning(curate_spectra(data = long_data,
+                                clusters_regex = "IgGI1",
+                                min_ppm_deviation = 10000,
+                                max_ppm_deviation = 10000,
+                                max_ipq = 10000,
+                                min_sn = 0,
+                                group_to_filter = "Spike",
+                                sample_type_to_filter = "CN"),
+               "None of the spectra passed curation\\.")
+  # On the other hand, when criteria are really strict, none of the spectra pass
+  # either.
+  expect_warning(curate_spectra(data = long_data,
+                                clusters_regex = "IgGI1",
+                                min_ppm_deviation = 0,
+                                max_ppm_deviation = 0,
+                                max_ipq = 0,
+                                min_sn = 10000,
+                                group_to_filter = "Spike",
+                                sample_type_to_filter = "CN"),
+                 "None of the spectra passed curation\\.")
+})
+
