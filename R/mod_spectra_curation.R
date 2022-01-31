9f83f30138b56c09b7ad82bac5bb03fa625d2415
Sterre Siekman <s.l.siekman@lumc.nl>
2022-01-31 14:24:40 +0100

    Made curate_spectra actionButton functional.

--- R/mod_spectra_curation.R
+++ R/mod_spectra_curation.R
@@ -37,7 +37,7 @@ mod_spectra_curation_ui <- function(id){
                           max = 50,
                           value = c(-20, 20)
               ),
-              numericInput(ns("IPQ"),
+              numericInput(ns("ipq"),
                            "Max. IPQ value:",
                            value = 0.2,
                            step = 0.1),
@@ -88,8 +88,14 @@ mod_spectra_curation_server <- function(id, results_data_import){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
     
+    # Creating a reactiveValues object in which reactiveVals from this module
+    # can be saved: (reactiveVals are often easier to work with than reactive
+    # expressions for some reason)
     x <- reactiveValues()
     
+    # If data_incl_metadata exists it is assigned to x$data, otherwise
+    # data_incl_plate_design is assigned to x$data. x$data (not the reactives
+    # from the previous module) will be used from this point in the module.
     observe({
       if (isTruthy(results_data_import$data_incl_metadata())){
         x$data <- results_data_import$data_incl_metadata()
@@ -109,6 +115,8 @@ mod_spectra_curation_server <- function(id, results_data_import){
       return(cluster_inputIds)
     })
     
+    # Create textInputs for the clusters. The number of inputs created is the
+    # same as the value of input$n_clusters.
     output$clusters <- renderUI({
       req(cluster_inputIds())
         purrr::imap(cluster_inputIds(),
@@ -120,6 +128,9 @@ mod_spectra_curation_server <- function(id, results_data_import){
                       ))
     })
     
+    # The selection menu for input$cut_off_basis is updated so that the choices
+    # are all combinations of sample_types and groups that are present in the
+    # data.
     observeEvent(x$data, {
       combinations <- expand.grid(sample_type = unique(x$data$sample_type),
                              group = unique(x$data$group))
@@ -134,6 +145,41 @@ mod_spectra_curation_server <- function(id, results_data_import){
                    unique(x$data$group))
       updateSelectInput(inputId = "cut_off_basis",
                         choices = options)
+    })
+    
+    observeEvent(input$curate_spectra, {
+      # Get the values of the cluster textInputs:
+      clusters_regex <- purrr::map(cluster_inputIds(),
+                                  ~ input[[.x]])
+      
+      # Extract the group out of input$cut_off_basis that was selected by the
+      # user:
+      group_to_filter <- stringr::str_extract(string = input$cut_off_basis,
+                                              pattern = paste0(unique(x$data$group),
+                                                               collapse = "|"))
+      
+      # Extract the sample_type out of input$cut_off_basis that was selected by
+      # the user as cut_off_basis:
+      sample_type_to_filter <- stringr::str_extract(string = input$cut_off_basis,
+                                                    pattern = paste0(
+                                                      unique(x$data$sample_type),
+                                                      collapse = "|"))
+      
+      # Perform spectra curation:
+      x$data_spectra_curated <- curate_spectra(
+        data = x$data,
+        clusters_regex = clusters_regex,
+        min_ppm_deviation = input$mass_accuracy[1],
+        max_ppm_deviation = input$mass_accuracy[2],
+        max_ipq = input$ipq,
+        min_sn = input$sn,
+        group_to_filter = group_to_filter,
+        sample_type_to_filter = sample_type_to_filter)
+      
+      # Filter out all spectra that didn't pass curation:
+      x$curated_spectra <- x$data_spectra_curated %>% 
+        dplyr::filter(passed_curation == TRUE)
+      
     })
     
     output$p <- renderTable({shinipsum::random_table(3, 3)})
