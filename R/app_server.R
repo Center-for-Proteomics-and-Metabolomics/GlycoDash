d0adeb758176746474e9101a94fbabc9b3873108
Sterre Siekman <s.l.siekman@lumc.nl>
2022-01-31 12:23:26 +0100

    Made communication between modules functional

--- R/app_server.R
+++ R/app_server.R
@@ -7,10 +7,9 @@
 app_server <- function( input, output, session ) {
   # Your application server logic 
   
-  # Using the "stratégie du petit r" for communication between modules:
-  r <- reactiveValues()
-  mod_data_import_server("data_import_ui_1")
-  mod_spectra_curation_server("spectra_curation_ui_1")
+  results_data_import <- mod_data_import_server("data_import_ui_1")
+  mod_spectra_curation_server(id = "spectra_curation_ui_1", 
+                              results_data_import = results_data_import)
   mod_analyte_curation_server("analyte_curation_ui_1")
   mod_normalization_server("normalization_ui_1")
   mod_derived_traits_server("derived_traits_ui_1")
