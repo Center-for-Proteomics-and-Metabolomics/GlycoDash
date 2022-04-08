923b7618b74fc5714cfeb3a8c0578ca8ba9cd026
Sterre Siekman <s.l.siekman@lumc.nl>
2022-04-08 23:15:31 +0200

    Moved Report.Rmd to inst/app/www folder

--- R/mod_export.R
+++ R/mod_export.R
@@ -185,7 +185,7 @@ mod_export_server <- function(id,
                        )
         
         temp_report <- file.path(tempdir(), "Report.Rmd")
-        file.copy("Report.Rmd", temp_report, overwrite = TRUE)
+        file.copy(system.file("www", "Report.Rmd"), temp_report, overwrite = TRUE)
         
         rmarkdown::render(input = temp_report,
                           output_file = file,
