5a98e9e665a3d58f5749ac26df877ee3c654cfb9
Sterre Siekman <s.l.siekman@lumc.nl>
2022-07-12 16:50:20 +0200

    Created a visualize_repeatability2 function that works with plotly and has no secondary y-axis

--- R/fct_repeatability.R
+++ R/fct_repeatability.R
@@ -32,24 +32,25 @@ visualize_repeatability <- function(repeatability_data) {
                                                        hjust = 1),
                    #text = ggplot2::element_text(size = 16),
                    panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=0.5)) +
-    ggplot2::geom_point(ggplot2::aes(x = analyte, 
-                                     y = RSD,
-                                     group = plate,
-                                     fill = plate,
-                                     text = paste("RSD:",
-                                                   signif(RSD, 3),
-                                                   "%")),
-                        shape = 21,
-                        stroke = 0.5,
-                        position = ggplot2::position_dodge(width = .9),
-                        #show.legend = FALSE
-                        ) +
+    # ggplot2::geom_point(ggplot2::aes(x = analyte, 
+    #                                  y = RSD,
+    #                                  group = plate,
+    #                                  fill = plate,
+    #                                  text = paste("RSD:",
+    #                                                signif(RSD, 3),
+    #                                                "%")),
+    #                     shape = 21,
+    #                     stroke = 0.5,
+    #                     position = ggplot2::position_dodge(width = .9),
+    #                     #show.legend = FALSE
+    #                     ) +
     ggplot2::scale_y_continuous(name = "Relative abundance (%)",
-                                labels = function(x) paste0(x, "%"),
-                                sec.axis = ggplot2::sec_axis(~ . / rescale_RSD_with, 
-                                                             name = "RSD (%)",
-                                                             labels = function(x) paste0(x,
-                                                                                         "%"))) +
+                                labels = function(x) paste0(x, "%")#,
+                                # sec.axis = ggplot2::sec_axis(~ . / rescale_RSD_with, 
+                                #                              name = "RSD (%)",
+                                #                              labels = function(x) paste0(x,
+                                #                                                          "%"))
+                                ) +
     ggplot2::scale_x_discrete(name = "Analyte") +
     ggplot2::scale_fill_brewer(palette = "Set2", 
                                name = "Plate") +
@@ -97,4 +98,65 @@ calculate_repeatability_stats <- function(data,
                      across(cluster)) %>% 
     dplyr::ungroup(.)
   
+}
+
+visualize_repeatability2 <- function(repeatability_data) {
+  
+  # range_av_abundance <- max(repeatability_data$average_abundance,
+  #                           na.rm = TRUE) - min(repeatability_data$average_abundance,
+  #                                               na.rm = TRUE)
+  # range_RSD <- max(repeatability_data$RSD,
+  #                  na.rm = TRUE) - min(repeatability_data$RSD,
+  #                                      na.rm = TRUE)
+  # rescale_RSD_with <- range_av_abundance / range_RSD
+  
+  plot <- repeatability_data %>% 
+    #dplyr::mutate(RSD = RSD * rescale_RSD_with) %>% 
+    ggplot2::ggplot() +
+    ggplot2::geom_col(ggplot2::aes(x = analyte, 
+                                   y = average_abundance, 
+                                   fill = plate,
+                                   text = paste("Analyte:",
+                                                analyte,
+                                                "\nPlate:",
+                                                plate,
+                                                "\nAverage relative abundance:",
+                                                signif(average_abundance, 3),
+                                                "%")),
+                      position = "dodge") +
+    ggplot2::theme_classic() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45,
+                                                       hjust = 1),
+                   #text = ggplot2::element_text(size = 16),
+                   panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=0.5)) +
+    ggplot2::geom_point(ggplot2::aes(x = analyte,
+                                     y = RSD,
+                                     group = plate,
+                                     fill = plate,
+                                     text = paste("Analyte:",
+                                                  analyte,
+                                                  "\nPlate:",
+                                                  plate,
+                                                  "\nRSD:",
+                                                  signif(RSD, 3),
+                                                  "%"
+                                     )),
+                      shape = 21,
+                      stroke = 0.5,
+                      position = ggplot2::position_dodge(width = .9),
+                      #show.legend = FALSE
+                      ) +
+  ggplot2::scale_y_continuous(name = "Relative abundance (%)",
+                              labels = function(x) paste0(x, "%")#,
+                              # sec.axis = ggplot2::sec_axis(~ . / rescale_RSD_with, 
+                              #                              name = "RSD (%)",
+                              #                              labels = function(x) paste0(x,
+                              #                                                          "%"))
+  ) +
+    ggplot2::scale_x_discrete(name = "Analyte") +
+    ggplot2::scale_fill_brewer(palette = "Set2", 
+                               name = "Plate") +
+    ggplot2::facet_wrap(~cluster)
+  
+  return(plot)
 }
