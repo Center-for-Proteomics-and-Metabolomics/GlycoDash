8f1d864480fb21362041b29bd8bdbaf08165df50
Sterre Siekman <s.l.siekman@lumc.nl>
2022-09-15 14:52:58 +0200

    Move functions from fct_data_import
    All remaining functions in fct_data_import.R were moved to the right
    files and fct_data_import.R was deleted.

--- R/fct_add_metadata.R
+++ R/fct_add_metadata.R
@@ -79,3 +79,137 @@ prep_metadata <- function(input,
                                    by = "sample_id")
   return(merged_metadata)
 }
+
+#'Convert serial dates with comments to dates
+#'
+#'\code{date_with_text()} tries to convert a vector with serial dates (given as
+#'character strings) to objects of the class "Date". In Excel dates are stored
+#'as serial numbers. These serial dates are the number of days passed since a
+#'set origin date. When Excel data is read into R the serial dates are usually
+#'recognized as dates and converted to objects of class "Date". However, when in
+#'Excel a cell contains both a date and a comment, the column containing this
+#'cell is no longer recognized as class "Date" and is instead converted to class
+#'"Character". \code{date_with_text()} attempts to convert serial dates (given
+#'as character strings) to objects of class "Date". If some serial dates include
+#'comments, the function instead converts the serial dates to character strings
+#'with the dates formatted as "year-month-day". \strong{Important: This function
+#'can only be used within the function \code{\link[dplyr]{across}} from the
+#'dplyr package!}
+#'
+#'@param date_text_values A column in a dataframe or tibble with serial dates in
+#'  the form of character strings.
+#'@param origin The origin date used to calculate the serial dates. Defaults to
+#'  1899-12-30 (year-month-day) which is the origin used in Excel on Windows for
+#'  all dates from 1900-01-01 and on.
+#'
+#'@return
+#'@export
+#'
+#' @section Credits:
+#' This function was based on a solution found on StackOverflow.\cr
+#' Question asked by Ryan C. Thompson:\cr
+#' \url{https://stackoverflow.com/questions/63027147/how-to-read-in-a-date-column-from-an-excel-sheet-with-ambiguous-formats-into-r}\cr
+#' Solution provided by Rui Barradas:\cr
+#' \url{https://stackoverflow.com/users/8245406/rui-barradas}
+#'
+#' @examples
+#' path <- system.file("extdata",
+#'                     "for_tests",
+#'                     "Dates_with_comment.xlsx",
+#'                     package = "glycodash")
+#'
+#'# Dates with comments in Excel are usually interpreted
+#'# as character strings in R:
+#'
+#' dates_with_comment <- readxl::read_excel(path)
+#'
+#' print(dates_with_comment)
+#'
+#'# # A tibble: 6 x 1
+#'# date
+#'# <chr>
+#'# 1 44603
+#'# 2 44604
+#'# 3 13-02-2022 oh no a comment
+#'# 4 44606
+#'# 5 44607
+#'# 6 44608
+#'
+#' dates_with_comment <- dates_with_comment %>%
+#'    dplyr::mutate(dplyr::across(.cols = date,
+#'                                .fns = date_with_text))
+#'
+#'# Because text is found within the dates
+#'# column the following message is shown:
+#'# "Some date entries in date contain text.
+#'# Output will have class character."
+#'
+#'print(dates_with_comment)
+#'
+#' # # A tibble: 6 x 1
+#' # date
+#' # <chr>
+#' # 1 2022-02-11
+#' # 2 2022-02-12
+#' # 3 13-02-2022 oh no a comment
+#' # 4 2022-02-14
+#' # 5 2022-02-15
+#' # 6 2022-02-16
+#'
+#' path <- system.file("extdata",
+#'                     "for_tests",
+#'                     "Dates.xlsx",
+#'                     package = "glycodash")
+#'
+#' # Forcing read_excel() to read the dates in as character strings 
+#' # (even though there are no comments in this file) for 
+#' # demonstration purposes:
+#' dates <- readxl::read_excel(path,
+#'                             col_types = "text")
+#'
+#' print(dates)
+#'
+#'# # A tibble: 6 x 1
+#'# date
+#'# <chr>
+#'# 1 44603
+#'# 2 44604
+#'# 3 44605
+#'# 4 44606
+#'# 5 44607
+#'# 6 44608
+#'
+#' dates <- dates %>%
+#'    dplyr::mutate(dplyr::across(.cols = date,
+#'                                .fns = date_with_text))
+#'                                
+#' # When no text is found within the dates column,
+#' # the serial dates are converted to objects of class "date":
+#' print(dates)
+#'
+#' # # A tibble: 6 x 1
+#' # date
+#' # <date>
+#' # 1 2022-02-11
+#' # 2 2022-02-12
+#' # 3 2022-02-13
+#' # 4 2022-02-14
+#' # 5 2022-02-15
+#' # 6 2022-02-16
+#'           
+date_with_text <- function(date_text_values, origin = "1899-12-30"){
+  no_nas <- date_text_values[!is.na(date_text_values)]
+  test <- suppressWarnings(as.numeric(no_nas))
+  num <- suppressWarnings(as.numeric(date_text_values))
+  dates <- as.Date(num, origin = origin)
+  if(anyNA(test)){
+    dates <- as.character(dates)
+    dates[is.na(num)] <- as.character(date_text_values[is.na(num)])
+    rlang::warn(class = "text_in_dates",
+                message = paste("Some date entries in", 
+                                tryCatch(dplyr::cur_column(),
+                                         error = function(e) {}), 
+                                "contain text. Output will have class character."))
+  }
+  return(dates)
+}
--- R/utils_general.R
+++ R/utils_general.R
@@ -62,3 +62,84 @@ try_call <- function(uncalled_reactive_expression) {
     }
   )
 }
+
+#' Load R-objects and assign them a new name
+#'
+#' When you load an R-object with \code{\link[base]{load}} the object keeps the
+#' name it had at the moment that it was saved. Sometimes it's more convenient
+#' to assign a new name to the object when it is being loaded.
+#' \code{load_and_assign()} allows you to do that.
+#' 
+#' @section Credits:
+#' The code for this function was found on StackOverflow.\cr
+#' Question asked by Ryan C. Thompson:\cr
+#' \url{https://stackoverflow.com/questions/5577221/how-can-i-load-an-object-into-a-variable-name-that-i-specify-from-an-r-data-file}\cr
+#' Solution provided by ricardo:\cr
+#' \url{https://stackoverflow.com/users/1453172/ricardo}
+#' 
+#' @param file_path The path to the R-object that you want to load.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' old_name <- c("This is a random vector",
+#'               "to serve as an example")
+#'
+#' # Creating a temporary directory:
+#' file_path <- file.path(tempdir(),
+#'                        "R_object.rds")
+#'
+#' # Saving the vector as an R-object to the temporary directory:
+#' save(old_name,
+#'      file = file_path)
+#'
+#' # Loading the R object and assigning a new name to it:
+#' new_name <- load_and_assign(file_path)
+#' 
+load_and_assign <- function(file_path){
+  tryCatch(
+    load(file_path),
+    error = function(e) {
+      
+    },
+    warning = function(w) { 
+      stop(paste("Check if the file_path was not misspelled.",
+                 "Was the R-object you're trying to load saved with the function saveRDS()?",
+                 "load_and_assign only works with objects saved with the function save()"))
+    })
+  get(ls()[ls() != "file_path"])
+}
+
+
+comma_and <- function(string_components) {
+  comma_string <- paste(string_components, collapse = ", ")
+  comma_and_string <- sub(pattern = ",\\s([^,]+)$", 
+                          replacement = " and \\1",
+                          x = comma_string)
+  return(comma_and_string)
+}
+
+comma_or <- function(string_components) {
+  comma_string <- paste(string_components, collapse = ", ")
+  comma_and_string <- sub(pattern = ",\\s([^,]+)$", 
+                          replacement = " or \\1",
+                          x = comma_string)
+  return(comma_and_string)
+}
+
+is_truthy <- function(x) {
+  valid <- tryCatch(
+    expr = {
+      x
+      TRUE
+    },
+    error = function(e) {
+      FALSE
+    })
+  if (valid) {
+    return(isTruthy(x))
+  } else {
+    return(FALSE)
+  }
+}
