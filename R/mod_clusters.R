16e8bb30d7f4e5021f5d5799261e125dc502b8b0
stainawarijar <steinar-gijze@hotmail.nl>
2023-06-22 17:40:37 +0200

    SILuMAb inputIds, check keywords

--- R/mod_clusters.R
+++ R/mod_clusters.R
@@ -20,20 +20,32 @@ mod_clusters_ui <- function(id){
       choices = c("Yes", "No"),
       selected = "No"
     ),
-    textInput(
-      ns("silumab_glycopeptides"),
-      "By what word/letters within the analyte name can the SILuMAb glycopeptides be recognized?"
+    fluidRow(
+      column(width = 6,
+        textInput(ns("silumab_cluster_glyco"),
+          "By what word/letters within the analyte name can the SILuMAb glycopeptides be recognized?"
+        ),
+        textInput(ns("silumab_cluster_GPS"),
+          "By what word/letters within the analyte name can the SILuMAb peptide GPSVFPLAPSSK be recognized?"
+        ),
+        textInput(ns("silumab_cluster_TTP"),
+          "By what word/letters within the analyte name can the SILuMAb peptide TTPVLDSDGSFFLYSK be recognized?"
+        )
+      ),
+      column(width = 6,
+        textInput(ns("IgG1_cluster_glyco"),
+          "By what word/letters within the analyte name can the IgG1 glycopeptides be recognized?"
+        ),
+        textInput(ns("IgG1_cluster_GPS"),
+          "By what word/letters within the analyte name can the IgG1 peptide GPSVFPLAPSSK be recognized?"
+        ),
+        textInput(ns("IgG1_cluster_TTP"),
+          "By what word/letters within the analyte name can the IgG1 peptide TTPVLDSDGSFFLYSK  be recognized?"
+        )
+      )
     ),
-    textInput(
-      ns("silumab_peptide1"),
-      "By what word/letters within the analyte name can the SILuMAb peptide GPSVFPLAPSSK be recognized?"
-    ),
-    textInput(
-      ns("silumab_peptide2"),
-      "By what word/letters within the analyte name can the SILuMAb peptide TTPVLDSDGSFFLYSK  be recognized?"
-    ),
-    numericInput(ns("n_clusters"), 
-                 "How many natural Ig clusters does your data contain?",
+    numericInput(ns("n_clusters"),
+                 "How many peptide clusters does your data contain?",
                  value = 1,
                  min = 1,
                  max = 25,
@@ -43,6 +55,7 @@ mod_clusters_ui <- function(id){
                  "Define the clusters")
   )
 }
+
     
 #' clusters Server Functions
 #'
@@ -53,14 +66,29 @@ mod_clusters_server <- function(id, LacyTools_summary){
     
 # Create textInputs -------------------------------------------------------
 
-    # Create inputIds for the cluster textInputs based on the
-    # value of input$n_clusters:
+    # Create inputIds for the cluster textInputs based on the value of input$n_clusters.
     cluster_inputIds <- reactive({
       req(input$n_clusters)
-      cluster_inputIds <- purrr::map(seq_len(input$n_clusters),
-                                     ~ paste0("cluster", .x))
-      return(cluster_inputIds)
+      purrr::map(seq_len(input$n_clusters),
+                 ~ paste0("cluster", .x))
     })
+    
+    # Create inputIds for the SILuMAb clusters.
+    silumab_inputIds <- reactive({
+      req(input$contains_silumab == "Yes")
+      list("silumab_cluster_glyco", "silumab_cluster_GPS", "silumab_cluster_TTP")
+    })
+    
+    # Combine cluster_inputIds and silumab_inputIds if applicable
+    inputIds <- reactive({
+      req(cluster_inputIds())
+      if (is_truthy(silumab_inputIds())) {
+        c(silumab_inputIds(), cluster_inputIds())
+      } else {
+        cluster_inputIds()
+      }
+    })
+    
     
     # Create textInputs for the clusters. The number of inputs created is the
     # same as the value of input$n_clusters.
@@ -75,57 +103,74 @@ mod_clusters_server <- function(id, LacyTools_summary){
                   ))
     })
     
-
-# Show SILuMAb input only when user selects "Yes"
-observe({
-  shinyjs::toggle("silumab_glycopeptides", input$contains_silumab == "Yes")
-  shinyjs::toggle("silumab_peptide1", input$contains_silumab == "Yes")
-  shinyjs::toggle("silumab_peptide2", input$contains_silumab == "Yes")
-})
+    # Show SILuMAb input only when user selects "Yes".
+    observe({
+      inputs_to_toggle <- c("silumab_cluster_glyco", "silumab_cluster_GPS", "silumab_cluster_TTP",
+                            "IgG1_cluster_glyco", "IgG1_cluster_GPS", "IgG1_cluster_TTP")
+      for (i in inputs_to_toggle) {
+        shinyjs::toggle(i, input$contains_silumab == "Yes")
+      }
+    })
     
-    
+    # Change label of input$n_clusters when the data contains SILuMAb
+    observeEvent(input$contains_silumab, {
+      updateNumericInput(session,
+        "n_clusters",
+        label = ifelse(
+          input$contains_silumab == "Yes",
+          "How many additional peptide clusters does your data contain?",
+          "How many peptide clusters does your data contain?"
+        ))
+    })
+  
     
 # Keyword checks ----------------------------------------------------------
 
     cluster_keywords_found <- reactive({
-      req(cluster_inputIds())
+      # req(cluster_inputIds())
+      req(inputIds())
       
       unique_analytes_in_data <- unique(LacyTools_summary()$analyte)
-      
+
       keywords_found <- purrr::map_lgl(
-        cluster_inputIds(),
-        function(cluster_inputId) {
-          req(!is.null(input[[cluster_inputId]]))
-          
-          if (input[[cluster_inputId]] != "") {
+        inputIds(),
+        function(inputId) {
+          req(!is.null(input[[inputId]]))
+          if (input[[inputId]] != "") {
             find_cluster_keyword_match(unique_analytes = unique_analytes_in_data,
-                                       cluster_keyword = input[[cluster_inputId]])
+                                       cluster_keyword = input[[inputId]])
           } else {
             TRUE
           }
-          
         })
       
-      names(keywords_found) <- cluster_inputIds()
-      
+      # names(keywords_found) <- cluster_inputIds()
+      names(keywords_found) <- inputIds()
+
       return(keywords_found)
     })
     
     
     keywords <- reactive({
-      req(purrr::map(cluster_inputIds(),
-                     ~input[[.x]]))
+      # req(purrr::map(cluster_inputIds(),
+      #                ~input[[.x]]))
+      # 
+      # purrr::map(cluster_inputIds(),
+      #            ~input[[.x]])
+      req(purrr::map(inputIds(),
+                     ~ input[[.x]]))
       
-      purrr::map(cluster_inputIds(),
-                 ~input[[.x]])
+      purrr::map(inputIds(),
+                 ~ input[[.x]])
     })
     
     
     # Display feedback based on the checks that were performed:
     observe({
       req(!is.null(cluster_keywords_found()))
-      
-      purrr::map(cluster_inputIds(),
+      # purrr::map(cluster_inputIds(),
+      #            ~ shinyFeedback::hideFeedback(.x))
+      purrr::map(inputIds(),
                  ~ shinyFeedback::hideFeedback(.x))
       
       if (any(!cluster_keywords_found())) {
