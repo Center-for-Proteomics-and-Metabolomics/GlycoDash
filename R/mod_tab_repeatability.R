5b2823364d5516a4e0169b14f3a4147c6d70a508
stainawarijar <steinar-gijze@hotmail.nl>
2024-07-12 11:33:25 +0200

    Make plots resizable

--- R/mod_tab_repeatability.R
+++ R/mod_tab_repeatability.R
@@ -38,7 +38,7 @@ mod_tab_repeatability_ui <- function(id){
             status = "primary",
             column(
               width = 9,
-              shinyjqui::jqui_resizable(plotly::plotlyOutput(ns("plot"), width = "1300px")),
+              shinyjqui::jqui_resizable(plotly::plotlyOutput(ns("plot"))),
               tabsetPanel(id = ns("plot_tabs"))
             ),
             column(
