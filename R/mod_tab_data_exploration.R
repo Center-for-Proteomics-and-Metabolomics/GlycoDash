69e7218ca60b1429452ee1538c33c3b49f87f4d0
sgijze <s.gijze@lumc.nl>
2026-07-13 09:40:50 +0200

    Clean up data exploration code

--- R/mod_tab_data_exploration.R
+++ R/mod_tab_data_exploration.R
@@ -7,7 +7,7 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
-mod_tab_data_exploration_ui <- function(id){
+mod_tab_data_exploration_ui <- function(id) {
   ns <- NS(id)
   tagList(
     br(),
@@ -19,32 +19,42 @@ mod_tab_data_exploration_ui <- function(id){
           solidHeader = FALSE,
           status = "primary",
           collapsible = TRUE,
-          selectizeInput(ns("plot_type"),
-                         choices = c("Boxplot",
-                                     "Scatter plot",
-                                     "Histogram"),
-                         label = "What type of plot do you want?"),
-          selectizeInput(ns("filter"),
-                         choices = "",
-                         selected = NULL,
-                         multiple = TRUE,
-                         label = "Choose which sample types should be excluded from the plot:"),
-          selectizeInput(ns("yvar"),
-                         choices = "",
-                         selected = NULL,
-                         label = "Choose what variable should be on the y-axis:"),
-          selectizeInput(ns("xvar"),
-                         choices = "",
-                         selected = NULL,
-                         label = "Choose what variable should be on the x-axis:"),
-          selectizeInput(ns("facets"),
-                         choices = "",
-                         selected = NULL,
-                         label = "Choose what variable to facet by:"),
-          selectizeInput(ns("color"),
-                         choices = "",
-                         selected = NULL,
-                         label = "Choose what variable to color by:")
+          selectizeInput(
+            ns("plot_type"),
+            choices = c("Boxplot", "Scatter plot", "Histogram"),
+            label = "Type of plot:"
+          ),
+          selectizeInput(
+            ns("filter"),
+            choices = "",
+            selected = NULL,
+            multiple = TRUE,
+            label = "Sample types to exclude from figure:"
+          ),
+          selectizeInput(
+            ns("yvar"),
+            choices = "",
+            selected = NULL,
+            label = "y-axis variable:"
+          ),
+          selectizeInput(
+            ns("xvar"),
+            choices = "",
+            selected = NULL,
+            label = "x-axis variable:"
+          ),
+          selectizeInput(
+            ns("facets"),
+            choices = "",
+            selected = NULL,
+            label = "Variable to facet by:"
+          ),
+          selectizeInput(
+            ns("color"),
+            choices = "",
+            selected = NULL,
+            label = "Variable to color by:"
+          )
         )
       ),
       br(),
@@ -56,11 +66,16 @@ mod_tab_data_exploration_ui <- function(id){
   )
 }
     
+
 #' tab_data_exploration Server Functions
 #'
 #' @noRd 
-mod_tab_data_exploration_server <- function(id, my_data, trigger){
-  moduleServer( id, function(input, output, session){
+mod_tab_data_exploration_server <- function(
+    id, 
+    my_data, 
+    trigger  
+  ) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
     # Hide y-variable when user selects histogram.
@@ -73,80 +88,97 @@ mod_tab_data_exploration_server <- function(id, my_data, trigger){
     
     observe({
       req(my_data())
-      updateSelectizeInput(inputId = "yvar",
-                           choices = c("", colnames(my_data())))
-      updateSelectizeInput(inputId = "xvar",
-                           choices = c("", colnames(my_data())))
-      updateSelectizeInput(inputId = "facets",
-                           choices = c("", colnames(my_data())))
-      updateSelectizeInput(inputId = "color",
-                           choices = c("", colnames(my_data())))
-      updateSelectizeInput(inputId = "filter",
-                           choices = c("", unique(my_data()["sample_type"])))
+      
+      updateSelectizeInput(
+        inputId = "yvar",
+        choices = c("", colnames(my_data()))
+      )
+      updateSelectizeInput(
+        inputId = "xvar",
+        choices = c("", colnames(my_data()))
+      )
+      updateSelectizeInput(
+        inputId = "facets",
+        choices = c("", colnames(my_data()))
+      )
+      updateSelectizeInput(
+        inputId = "color",
+        choices = c("", colnames(my_data()))
+      )
+      updateSelectizeInput(
+        inputId = "filter",
+        choices = c("", unique(my_data()["sample_type"]))
+      )
     }) %>% bindEvent(trigger()) # Only once the trigger has become TRUE (and 
     # thus when the selectizeInputs have been rendered) are the selectizeInputs
     # updated.
+    
     
     filtered_data <- reactive({
       req(my_data())
       
       if(isTruthy(input$filter)) {
-        filtered_data <- my_data() %>% 
+        my_data() %>% 
           dplyr::filter(!(sample_type %in% input$filter))
-      } else {
-        filtered_data <- my_data()
+      } 
+      else {
+        my_data()
       }
-      
-      return(filtered_data)
-      
     })
+    
     
     my_plot <- reactive({
       req(filtered_data())
       
       if (is_truthy(input$color)) {
         color <- input$color
-      } else { color <- NULL }
+      } 
+      else {
+        color <- NULL
+      }
       
       if (is_truthy(input$facets)) {
         facets <- input$facets
-      } else { facets <- NULL }
-      
-      if (input$plot_type == "Boxplot") {
-        req(input$xvar,
-            input$yvar)
-        my_boxplot(filtered_data(),
-                   xvar = input$xvar,
-                   yvar = input$yvar,
-                   color = color,
-                   facets = facets)
-      } else {
-        if (input$plot_type == "Scatter plot") {
-          req(input$xvar,
-              input$yvar)
-          my_scatter_plot(filtered_data(),
-                          xvar = input$xvar,
-                          yvar = input$yvar,
-                          color = color,
-                          facets = facets)
-        } else {
-          if (input$plot_type == "Histogram") {
-            req(input$xvar)
-            
-            my_histogram(filtered_data(),
-                         xvar = input$xvar,
-                         color = color,
-                         facets = facets)
-          }
-        }
+      } 
+      else {
+        facets <- NULL
       }
       
+      if (input$plot_type == "Boxplot") {
+        req(input$xvar, input$yvar)
+        my_boxplot(
+          filtered_data(),
+          xvar = input$xvar,
+          yvar = input$yvar,
+          color = color,
+          facets = facets
+        )
+      } 
+      else if (input$plot_type == "Scatter plot") {
+        req(input$xvar, input$yvar)
+        my_scatter_plot(
+          filtered_data(),
+          xvar = input$xvar,
+          yvar = input$yvar,
+          color = color,
+          facets = facets
+        )
+      }
+      else if (input$plot_type == "Histogram") {
+        req(input$xvar)
+        my_histogram(
+          filtered_data(),
+          xvar = input$xvar,
+          color = color,
+          facets = facets
+        )
+      }
     })
+    
     
     output$plot <- plotly::renderPlotly({
       
-      plotly_object <- plotly::ggplotly(my_plot(), 
-                                        tooltip = "text") 
+      plotly_object <- plotly::ggplotly(my_plot(), tooltip = "text") 
       
       if (input$plot_type == "Boxplot") {
         # Determine the number of facets n, the first n traces will correspond to
@@ -160,9 +192,10 @@ mod_tab_data_exploration_server <- function(id, my_data, trigger){
           hide_outliers(.) 
       }
       
-      return(plotly_object)
-      
+      plotly_object
     })
+    
+    
     
     return(list(
       plot = my_plot
@@ -171,8 +204,3 @@ mod_tab_data_exploration_server <- function(id, my_data, trigger){
   })
 }
     
-## To be copied in the UI
-# mod_tab_data_exploration_ui("tab_data_exploration_ui_1")
-    
-## To be copied in the server
-# mod_tab_data_exploration_server("tab_data_exploration_ui_1")
